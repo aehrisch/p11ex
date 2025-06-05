2b31ddd865e6b168320be9297167c347edea8fe9
Eric Knauel <eric.knauel@posteo.de>
2025-06-05 21:24:10 +0200

    test: complete yubikey digest test

--- test/p11ex/yubikey_test.exs
+++ test/p11ex/yubikey_test.exs
@@ -54,5 +54,22 @@ defmodule P11ex.YubikeyTest do
     {:ok, random} = Session.generate_random(session_pid, 16)
     assert is_binary(random)
     assert byte_size(random) == 16
+
+    algs = [
+      {:ckm_sha1, 20, :sha},
+      {:ckm_sha256, 32, :sha256},
+      {:ckm_sha384, 48, :sha384},
+      {:ckm_sha512, 64, :sha512}
+    ]
+
+    algs |> Enum.each(fn {mechanism, size, name} ->
+      :ok = Session.digest_init(session_pid, {mechanism})
+      {:ok, digest} = Session.digest(session_pid, random)
+
+      assert digest != nil
+      assert is_binary(digest)
+      assert byte_size(digest) == size
+      assert :crypto.hash(name, random) == digest
+    end)
   end
 end
