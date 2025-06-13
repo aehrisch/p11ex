7fc2b8a10f9157fc7bac62b5ed4895f7d08b8474
Eric Knauel <eric.knauel@posteo.de>
2025-06-13 21:27:16 +0200

    chore: try to fix curl download

--- scripts/download-pkcs11-headers.sh
+++ scripts/download-pkcs11-headers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-url=https://raw.githubusercontent.com/oasis-tcs/pkcs11/master/published/3-01/
+url=https://raw.githubusercontent.com/oasis-tcs/pkcs11/master/published/3-01
 files=(pkcs11.h pkcs11t.h pkcs11f.h)
 
 for file in "${files[@]}"; do
-  curl -z -o $1/$file $url/$file
+  curl -L -c --no-progress-meter -z -o $1/$file $url/$file
 done
