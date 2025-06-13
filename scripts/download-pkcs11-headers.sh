d83dacbf2e4f54db7017efaa8b398fbb00747ced
Eric Knauel <eric.knauel@posteo.de>
2025-06-13 21:23:25 +0200

    chore: fix pkcs11 header download

--- scripts/download-pkcs11-headers.sh
+++ scripts/download-pkcs11-headers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-# Download PKCS11 headers
-curl -z pkcs11.h -O https://raw.githubusercontent.com/oasis-tcs/pkcs11/master/published/3-01/pkcs11.h
-curl -z pkcs11t.h -O https://raw.githubusercontent.com/oasis-tcs/pkcs11/master/published/3-01/pkcs11t.h
-curl -z pkcs11f.h -O https://raw.githubusercontent.com/oasis-tcs/pkcs11/master/published/3-01/pkcs11f.h
+url=https://raw.githubusercontent.com/oasis-tcs/pkcs11/master/published/3-01/
+files=(pkcs11.h pkcs11t.h pkcs11f.h)
+
+for file in "${files[@]}"; do
+  curl -z -o $1/$file $url/$file
+done
