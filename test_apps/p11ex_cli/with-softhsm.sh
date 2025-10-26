67a7ce0677266575dd064b33f5662ccbf29cc772
Eric Knauel <eric.knauel@posteo.de>
2025-10-26 11:36:04 +0100

    chore: allow to use pkcs11-spy easily from run scripts

--- test_apps/p11ex_cli/with-softhsm.sh
+++ test_apps/p11ex_cli/with-softhsm.sh
@@ -6,8 +6,15 @@ export P11TOOL_PREFIX=${P11TOOL_PREFIX:-/opt/homebrew}
 export SOFTHSM2_CONF=cli-softhsm.conf
 
 export P11EX_PIN=1234
-export P11EX_MODULE=${P11EX_MODULE:-${SOFTHSM_PREFIX}/lib/softhsm/libsofthsm2.so}
 export P11EX_TOKEN_LABEL=Token_0
+
+if [ -n "${DO_SPY:-}" ]; then
+  echo "### Running with spy"
+  export P11EX_MODULE=/opt/homebrew/Cellar/opensc/0.26.1/lib/pkcs11-spy.so
+  export PKCS11SPY=${P11EX_SPY:-${SOFTHSM_PREFIX}/lib/softhsm/libsofthsm2.so}
+else
+  export P11EX_MODULE=${P11EX_MODULE:-${SOFTHSM_PREFIX}/lib/softhsm/libsofthsm2.so}
+fi
 
 if [ ! -d "token" ]; then
   echo "### token directory does not exist, initializing"
